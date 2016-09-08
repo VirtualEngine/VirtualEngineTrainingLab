2da4504e33e7ea7d2049f3cd774c90f088a677a7
Iain Brighton <Iain Brighton>
2016-09-08 15:52:24 +0100

    Fixes bug in vTrainingLabUserThumbnails path

--- DSCResources/vTrainingLabUserThumbnails/vTrainingLabUserThumbnails.schema.psm1
+++ DSCResources/vTrainingLabUserThumbnails/vTrainingLabUserThumbnails.schema.psm1
@@ -25,16 +25,16 @@ configuration vTrainingLabUserThumbnails {
     foreach ($user in $Users) {
 
         if ($PSBoundParameters.ContainsKey('Filename')) {
-            $thumbnailPhotoPath = '{0}.{1}' -f (Join-Path -Path $ThumbnailPhotoPath -ChildPath $Filename), $Extension;
+            $userThumbnailPhotoPath = '{0}.{1}' -f (Join-Path -Path $ThumbnailPhotoPath -ChildPath $Filename), $Extension;
         }
         else {
-            $thumbnailPhotoPath = '{0}.{1}' -f (Join-Path -Path $ThumbnailPhotoPath -ChildPath $user.SamAccountName), $Extension;
+            $userThumbnailPhotoPath = '{0}.{1}' -f (Join-Path -Path $ThumbnailPhotoPath -ChildPath $user.SamAccountName), $Extension;
         }
 
         vADUserThumbnailPhoto "$($user.SamAccountName)_Thumbnail" {
             DomainName = $DomainName;
             UserName = $user.SamAccountName;
-            ThumbnailPhoto = $thumbnailPhotoPath;
+            ThumbnailPhoto = $userThumbnailPhotoPath;
         }
 
     } #end foreach user
