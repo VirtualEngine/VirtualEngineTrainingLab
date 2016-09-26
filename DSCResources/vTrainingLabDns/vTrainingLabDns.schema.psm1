b63ad3c4ad5158e911df03e2b38920c85ea2a354
Iain Brighton <Iain Brighton>
2016-09-26 10:16:53 +0100

    Adds CatalogServicesHost CNAME Renames ITStoreHost to ServiceStoreHost

--- DSCResources/vTrainingLabDns/vTrainingLabDns.schema.psm1
+++ DSCResources/vTrainingLabDns/vTrainingLabDns.schema.psm1
@@ -3,42 +3,64 @@ configuration vTrainingLabDns {
         ## IP address used to calculate reverse lookup zone name
         [Parameter(Mandatory)]
         [System.String] $IPAddress,
-        
+
         ## Domain root FQDN used to AD paths
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $DomainName = 'lab.local',
-        
-        ## Hostname for itstore.$DomainName CNAME
-        [Parameter()] [ValidateNotNullOrEmpty()]
-        [System.String] $ITStoreHost = 'controller',
-        
+
+        ## Hostname for servicestore.$DomainName CNAME
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [Alias('ITStoreHost')]
+        [System.String] $ServiceStoreHost = 'controller',
+
+        ## Hostname for catalogservices.$DomainName CNAME
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $CatalogServicesHost = 'controller',
+
         ## Hostname for storefront.$DomainName CNAME
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $StorefrontHost = 'xenapp',
-        
+
         ## Hostname for storefront.$DomainName CNAME
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $SmtpHost = 'exchange'
     )
-    
+
     Import-DscResource -Module xDnsServer;
-    
-    if (-not $ITStoreHost.Contains('.')) {
-        $ITStoreHost = '{0}.{1}' -f $ITStoreHost, $DomainName;
+
+    if (-not $ServiceStoreHost.Contains('.')) {
+        $ServiceStoreHost = '{0}.{1}' -f $ServiceStoreHost, $DomainName;
     }
-    
-    xDnsRecord 'itstore_CName' {
+
+    xDnsRecord 'servicestore_CName' {
         Name = 'servicestore';
         Zone = $DomainName;
-        Target = $ITStoreHost;
+        Target = $ServiceStoreHost;
         Type = 'CName';
         Ensure = 'Present';
     }
-    
+
+    if (-not $CatalogServicesHost.Contains('.')) {
+        $CatalogServicesHost = '{0}.{1}' -f $CatalogServicesHost, $DomainName;
+    }
+
+    xDnsRecord 'catalogservices_CName' {
+        Name = 'catalogservices';
+        Zone = $DomainName;
+        Target = $CatalogServicesHost;
+        Type = 'CName';
+        Ensure = 'Present';
+    }
+
     if (-not $StorefrontHost.Contains('.')) {
         $StorefrontHost = '{0}.{1}' -f $StorefrontHost, $DomainName;
     }
-    
+
     xDnsRecord 'storefront_CName' {
         Name = 'storefront';
         Zone = $DomainName;
@@ -46,11 +68,11 @@ configuration vTrainingLabDns {
         Type = 'CName';
         Ensure = 'Present';
     }
-    
+
     if (-not $SmtpHost.Contains('.')) {
         $SmtpHost = '{0}.{1}' -f $SmtpHost, $DomainName;
     }
-    
+
     xDnsRecord 'smtp_CName' {
         Name = 'smtp';
         Zone = $DomainName;
@@ -58,7 +80,7 @@ configuration vTrainingLabDns {
         Type = 'CName';
         Ensure = 'Present';
     }
-    
+
     if ($IPAddress.EndsWith('.in-addr.arpa')) {
         xDnsServerPrimaryZone 'ReverseLookup' {
             Name = $IPAddress;
@@ -72,5 +94,5 @@ configuration vTrainingLabDns {
             DynamicUpdate = 'NonsecureAndSecure';
         }
     }
-    
+
 } #end configuration vTrainingLabDns
