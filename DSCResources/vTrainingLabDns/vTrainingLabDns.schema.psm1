9dd062a63dbfba5854291ceef22aded5a0cdc2a9
Iain Brighton <iainbrighton@hotmail.com>
2017-06-27 15:54:06 +0100

    Makes vTrainingLabDns host names options

--- DSCResources/vTrainingLabDns/vTrainingLabDns.schema.psm1
+++ DSCResources/vTrainingLabDns/vTrainingLabDns.schema.psm1
@@ -13,86 +13,116 @@ configuration vTrainingLabDns {
         [Parameter()]
         [ValidateNotNullOrEmpty()]
         [Alias('ITStoreHost')]
-        [System.String] $ServiceStoreHost = 'controller',
+        [System.String] $ServiceStoreHost,
 
         ## Hostname for catalogservices.$DomainName CNAME
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [System.String] $CatalogServicesHost = 'controller',
+        [System.String] $CatalogServicesHost,
 
         ## Hostname for storefront.$DomainName CNAME
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [System.String] $StorefrontHost = 'xenapp',
+        [System.String] $StorefrontHost,
 
         ## Hostname for storefront.$DomainName CNAME
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [System.String] $SmtpHost = 'exchange'
+        [System.String] $SmtpHost
     )
 
     Import-DscResource -Module xDnsServer;
 
-    if (-not $ServiceStoreHost.Contains('.')) {
-        $ServiceStoreHost = '{0}.{1}' -f $ServiceStoreHost, $DomainName;
-    }
+    if ($PSBoundParameters.ContainsKey('ServiceStoreHost')) {
 
-    xDnsRecord 'servicestore_CName' {
-        Name = 'servicestore';
-        Zone = $DomainName;
-        Target = $ServiceStoreHost;
-        Type = 'CName';
-        Ensure = 'Present';
-    }
+        if (-not $ServiceStoreHost.Contains('.')) {
 
-    if (-not $CatalogServicesHost.Contains('.')) {
-        $CatalogServicesHost = '{0}.{1}' -f $CatalogServicesHost, $DomainName;
-    }
+            $ServiceStoreHost = '{0}.{1}' -f $ServiceStoreHost, $DomainName;
+        }
 
-    xDnsRecord 'catalogservices_CName' {
-        Name = 'catalogservices';
-        Zone = $DomainName;
-        Target = $CatalogServicesHost;
-        Type = 'CName';
-        Ensure = 'Present';
-    }
+        xDnsRecord 'servicestore_CName' {
+            Name = 'servicestore';
+            Zone = $DomainName;
+            Target = $ServiceStoreHost;
+            Type = 'CName';
+            Ensure = 'Present';
+        }
 
-    if (-not $StorefrontHost.Contains('.')) {
-        $StorefrontHost = '{0}.{1}' -f $StorefrontHost, $DomainName;
-    }
+        ## Create unified RES management portal alias
+        xDnsRecord 'res_CName' {
+            Name = 'res';
+            Zone = $DomainName;
+            Target = $ServiceStoreHost;
+            Type = 'CName';
+            Ensure = 'Present';
+        }
 
-    xDnsRecord 'storefront_CName' {
-        Name = 'storefront';
-        Zone = $DomainName;
-        Target = $StorefrontHost;
-        Type = 'CName';
-        Ensure = 'Present';
-    }
+    } #end if ServiceStoreHost
+
+    if ($PSBoundParameters.ContainsKey('CatalogServicesHost')) {
+    
+        if (-not $CatalogServicesHost.Contains('.')) {
+            $CatalogServicesHost = '{0}.{1}' -f $CatalogServicesHost, $DomainName;
+        }
+
+        xDnsRecord 'catalogservices_CName' {
+            Name = 'catalogservices';
+            Zone = $DomainName;
+            Target = $CatalogServicesHost;
+            Type = 'CName';
+            Ensure = 'Present';
+        }
+    
+    } #end if CatalogServicesHost
+
+    if ($PSBoundParameters.ContainsKey('StorefrontHost')) {
+
+        if (-not $StorefrontHost.Contains('.')) {
+            $StorefrontHost = '{0}.{1}' -f $StorefrontHost, $DomainName;
+        }
+
+        xDnsRecord 'storefront_CName' {
+            Name = 'storefront';
+            Zone = $DomainName;
+            Target = $StorefrontHost;
+            Type = 'CName';
+            Ensure = 'Present';
+        }
+
+    } #end if StorefrontHost
 
     if (-not $SmtpHost.Contains('.')) {
         $SmtpHost = '{0}.{1}' -f $SmtpHost, $DomainName;
     }
 
-    xDnsRecord 'smtp_CName' {
-        Name = 'smtp';
-        Zone = $DomainName;
-        Target = $SmtpHost;
-        Type = 'CName';
-        Ensure = 'Present';
-    }
+    if ($PSBoundParameters.ContainsKey('SmtpHost')) {
+
+        xDnsRecord 'smtp_CName' {
+            Name = 'smtp';
+            Zone = $DomainName;
+            Target = $SmtpHost;
+            Type = 'CName';
+            Ensure = 'Present';
+        }
+
+    } #end if SmtpHost
 
     if ($IPAddress.EndsWith('.in-addr.arpa')) {
+
         xDnsServerPrimaryZone 'ReverseLookup' {
             Name = $IPAddress;
             DynamicUpdate = 'NonsecureAndSecure';
         }
+
     }
     else {
+
         $ipQuartets = $IPAddress.Split('.');
         xDnsServerPrimaryZone 'ReverseLookup' {
             Name = '{0}.{1}.{2}.in-addr.arpa' -f $ipQuartets[2], $ipQuartets[1], $ipQuartets[0];
             DynamicUpdate = 'NonsecureAndSecure';
         }
+
     }
 
 } #end configuration vTrainingLabDns
